3b7a74d559dc00bafadf47215bf27bfd3eb6f18f
Jakub Trnka <jakub.trnka@braiins.cz>
2020-01-22 23:43:41 +0100

    ckd_pub

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-mod bip32;
 mod base58;
+mod bip32;
 
 fn main() {
-//    let _message = Message::from_slice(&[1; 32]).expect("32 bytes");
-//    println!("{:x?}", base58_to_bytes(&String::from("AHoJ")).unwrap());
+    //    let _message = Message::from_slice(&[1; 32]).expect("32 bytes");
+    //    println!("{:x?}", base58_to_bytes(&String::from("AHoJ")).unwrap());
 }
-
